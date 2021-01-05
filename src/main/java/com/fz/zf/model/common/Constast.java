ff76656c1d026df1e5a19dcd21633bac0fa09981
heyongqi <heyongqi@isafetyworld.com>
2021-01-05 15:18:24 +0800

    公众号回复模板信息

--- src/main/java/com/fz/zf/model/common/Constast.java
+++ src/main/java/com/fz/zf/model/common/Constast.java
@@ -40,10 +40,41 @@ public class Constast {
     public static final String PHONE_CODE = "plate_phone_code_";
     public static final Integer PHONE_CODE_EXPIRE = 60 * 1; // 验证码1分钟过期
 
-
     /**
      * 公众号token 过期时间(2小时，微信规定的)
      */
     public static final String GZH_TOKEN = "gzh-token_";
+    public static final String GZH_MY_ID = "gzh-ovzQ2640QXMz1hEKJbKim2pcsDzg";
+
+    /**
+     * 公众号模板信息颜色
+     */
+    public static final String GZH_TEMP_COLOR = "#173177";
+    public static final String GZH_TEMP_OTHER_COLOR = "#FA3342";
+
+    /**
+     * 公众号是否已加载菜单
+     */
+    public static boolean INIT_MENU_FLAG = false;
+
+    /**
+     * 公众号设置菜单url
+     */
+    public static final String SET_MENU_URL = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=";
+
+    /**
+     * 公众号发送模板的id
+     */
+    public static final String SEND_TEMP_ID = "3s3ewkB7oD4xSh5IwqJIjm7mzlihu0rxk4IGI6_7v_k";
+
+    /**
+     * 公众号发送模板url
+     */
+    public static final String SEND_TEMP_URL = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=";
+
+    /**
+     * 公众号设置所属行业
+     */
+    public static final String SET_INDUSTRY_URL = "https://api.weixin.qq.com/cgi-bin/template/api_set_industry?access_token=";
 
 }
--- src/main/java/com/fz/zf/service/gzh/GzhService.java
+++ src/main/java/com/fz/zf/service/gzh/GzhService.java
@@ -7,11 +7,13 @@ import com.fz.zf.config.PropertiesBean;
 import com.fz.zf.model.common.Constast;
 import com.fz.zf.model.gzh.*;
 import com.fz.zf.model.gzh.menu.*;
+import com.fz.zf.util.BaiDuScanUtils;
 import com.fz.zf.util.RedisUtil;
 import com.fz.zf.util.ShaUtil;
 import com.thoughtworks.xstream.XStream;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -59,7 +61,9 @@ public class GzhService {
      * @return
      */
     public String processToStrXml(Map<String, String> map) {
-        fullMenus();
+        if (!Constast.INIT_MENU_FLAG) {
+            fullMenus();
+        }
 
         String msgType = map.get("MsgType");
         BaseMessage msg = null;
@@ -70,14 +74,14 @@ public class GzhService {
             case "text":
                 msg = dealText(map);
                 break;
-            case "image":
-                msg = dealImage(map);
-                break;
             case "news":
                 msg = dealNews(map);
                 break;
             case "event":
                 msg = dealEvent(map);
+                break;
+            case "image":
+                msg = dealImage(map);
                 break;
             default:
                 break;
@@ -103,6 +107,7 @@ public class GzhService {
                 if (StringUtils.equals(eventKey, "1")) {
                     return dealClickEvent(map);
                 }
+                break;
             case "VIEW":
 
                 break;
@@ -110,6 +115,20 @@ public class GzhService {
                 break;
         }
         return null;
+    }
+
+    // 回复图片中文字
+    private BaseMessage dealScanEvent(Map<String, String> map) {
+        org.json.JSONObject res = BaiDuScanUtils.getRes(map.get("PicUrl"));
+        JSONArray arr = res.getJSONArray("words_result");
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length(); i++) {
+            sb.append(arr.getJSONObject(i)
+                         .getString("words"));
+        }
+        TextMessage img_msg = new TextMessage(map, sb.toString());
+        return img_msg;
     }
 
     private BaseMessage dealClickEvent(Map<String, String> map) {
@@ -130,19 +149,22 @@ public class GzhService {
 
             List<SubButton> sub_button = new ArrayList<>();
             sub_button.add(new ViewSubButton("百度一下", "view", "http://www.soso.com/"));
-            sub_button.add(new PhotoSubButton("拍照或选择相册", "pic_photo_or_album", "21"));
+            sub_button.add(new PhotoSubButton("识别文字", "pic_photo_or_album", "21"));
 
             menuBtns.getButton()
                     .add(new TreeRootMenu("分类菜单", sub_button));
 
-            String url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=ACCESS_TOKEN";
-
+            String url = Constast.SET_MENU_URL + token;
             JSONObject json = JSONUtil.parseObj(menuBtns);
-            log.info("哈哈哈:" + json);
-            url = url.replace("ACCESS_TOKEN", token.toString());
             String resp = HttpUtil.post(url, json.toString());
+            JSONObject parseObj = JSONUtil.parseObj(resp);
+            if (parseObj.get("errcode")
+                        .toString()
+                        .equals("0")) {
+                Constast.INIT_MENU_FLAG = true;
+            }
 
-            log.info("结果:" + resp);
+            log.info("菜单设置结果:" + resp);
         }
     }
 
@@ -158,10 +180,13 @@ public class GzhService {
     }
 
     private BaseMessage dealImage(Map<String, String> map) {
+        /*// 回复固定图片
         Image image = new Image();
         image.setMediaId("1G_eoDqz4AIPzZxrzpEq1ogoZnha-R9gH0VDHb9GzsBy1zrd7_iLeRpuStCIOrSO");
         ImageMessage img_msg = new ImageMessage(map, image);
-        return img_msg;
+        return img_msg;*/
+
+        return dealScanEvent(map);
     }
 
     private BaseMessage dealText(Map<String, String> map) {
@@ -174,7 +199,7 @@ public class GzhService {
     }
 
     // 获取微信公众号token(使用时需判空)
-    private Object getToken() {
+    public Object getToken() {
         Map<String, Object> params = new HashMap<>();
         params.put("grant_type", "client_credential");
         params.put("appid", prop.gzhAppid);
@@ -195,5 +220,39 @@ public class GzhService {
         stream.processAnnotations(TextMessage.class);
         stream.processAnnotations(ImageMessage.class);
         stream.processAnnotations(NewsMessage.class);
+    }
+
+    // 设置所属行业
+    private void setIndustry() {
+        Object token = getToken();
+        String url = Constast.SET_INDUSTRY_URL + token;
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("industry_id1", "1"); // 互联网
+        map.put("industry_id2", "31"); // 消费品
+        JSONObject jsonObject = new JSONObject(map);
+        String post = HttpUtil.post(url, jsonObject.toString());
+        log.info("设置结果:" + post);
+    }
+
+    // 设置推送信息模板
+    private void sendTemplate(String toUser, String name, String address, String pay) {
+        Object token = getToken();
+        String url = Constast.SEND_TEMP_URL + token;
+        SendTempModel model = new SendTempModel();
+        model.setTemplate_id(Constast.SEND_TEMP_ID);
+        model.setTouser(toUser);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("first", new DataBean("您好，您本月物业费已出", Constast.GZH_TEMP_COLOR));
+        map.put("userName", new DataBean(name, Constast.GZH_TEMP_COLOR));
+        map.put("address", new DataBean(address, Constast.GZH_TEMP_COLOR));
+        map.put("pay", new DataBean(pay, Constast.GZH_TEMP_OTHER_COLOR));
+        map.put("remark", new DataBean("请尽快缴纳，如有疑问，请咨询狗蛋儿", Constast.GZH_TEMP_COLOR));
+        model.setData(map);
+        JSONObject json = JSONUtil.parseObj(model);
+        String resp = HttpUtil.post(url, json.toString());
+
+        log.info("模板信息发送结果:" + resp);
     }
 }
